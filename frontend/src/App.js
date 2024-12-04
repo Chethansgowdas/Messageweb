6dbd678eba04091315857d9e62430d52b1c15229
Anjan Kumar <anjanbhat1997@gmail.com>
2024-12-04 22:49:32 +0530

    removing unwanted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,7 @@ import Signupdiv from "./Components/pages/Signup";
 function App() {
   return (
     <div>
-      {/* <div class="container"> <h1>Welcome to My Page</h1> <div class="content"> <p>This is a sample div element.</p> </div> </div> */}
-      <div>
         <Signupdiv/>
-      </div>
     </div>
   );
 }
