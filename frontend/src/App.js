269490504bc28d67d9e96033b451af207dbfb14b
Anjan Kumar <anjanbhat1997@gmail.com>
2024-12-04 22:44:31 +0530

    Css for the login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,15 @@
 import './App.css';
+import React from "react";
+import Logindiv from "./Components/pages/login";
+import Signupdiv from "./Components/pages/Signup";
 
 function App() {
   return (
     <div>
       {/* <div class="container"> <h1>Welcome to My Page</h1> <div class="content"> <p>This is a sample div element.</p> </div> </div> */}
       <div>
-      <div class="container">
-        <div class="login-box">
-            <h2>Login</h2>
-            <form action="#">
-              <div class="input-box">
-                <input type="email" required/>
-                <label>Email</label>
-              </div>
-              <div class="input-box">
-                <input type="password" required/>
-                <label>Password</label>
-              </div>
-              <div class="forgot-pass">
-                <a href="#">Forgot your password?</a>
-              </div>
-              <button type="submit" class="btn">Login</button>
-                <div class="signup-link">
-                    <a href="#">Signup</a>
-                </div>
-            </form>
-        </div>
-
-        <span style="--i:0;"></span>
-
+        <Signupdiv/>
       </div>
-    </div>
     </div>
   );
 }
---
+++ frontend/src/Components/pages/Signup.js
@@ -0,0 +1,36 @@
+// Signupdiv.js
+import React from "react";
+
+const Signupdiv = () => {
+  return (
+    <div>
+        <div class="container">
+        <div class="login-box">
+            <h2>Signup</h2>
+            <form action="#">
+              <div class="input-box">
+                <input type="email" required/>
+                <label>Email</label>
+              </div>
+              <div class="input-box">
+                <input type="password" required/>
+                <label>Password</label>
+              </div>
+              <div class="input-box">
+                <input type="Confirmpassword" required/>
+                <label>Confirm Password</label>
+              </div>
+              <button type="submit" class="btn">Submit</button>
+            </form>
+        </div>
+
+        {Array.from({ length: 60 }, (_, i) => (
+          <span key={i} style={{ '--i': i + 1 }}></span>
+        ))}
+
+      </div>
+    </div>
+  );
+};
+
+export default Signupdiv;
