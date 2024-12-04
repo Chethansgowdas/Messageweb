bc256fb789a83d318a1f6b49dd530d17d93c3340
Anjan Kumar <anjanbhat1997@gmail.com>
2024-12-04 18:19:53 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="container"> <h1>Welcome to My Page</h1> <div class="content"> <p>This is a sample div element.</p> </div> </div>
     </div>
   );
 }
