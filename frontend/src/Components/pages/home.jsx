6e898dd95a442e544dd4ef8ef7c7a12c58b21d3c
Anjan Kumar <anjanbhat1997@gmail.com>
2024-12-05 00:44:50 +0530

    Navbar position Change

--- frontend/src/Components/pages/home.jsx
+++ frontend/src/Components/pages/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Homepagejs = () => {
   return (
-    <div>
+    <div class="top-left">
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
           <li class="nav-item" role="presentation">
             <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
