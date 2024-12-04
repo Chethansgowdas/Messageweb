feb2dd5698d39842dd23a83785f82f90c1520744
Anjan Kumar <anjanbhat1997@gmail.com>
2024-12-05 00:05:52 +0530

    navbar setup

--- frontend/src/Components/pages/home.jsx
+++ frontend/src/Components/pages/home.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Homepagejs = () => {
   return (
     <div>
-        
+        <nav class="nav nav-pills flex-column flex-sm-row">
+          <a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#">Active</a>
+          <a class="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
+          <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+          {/* <a class="flex-sm-fill text-sm-center nav-link disabled" aria-disabled="true">Disabled</a> */}
+        </nav>
     </div>
   );};
 
